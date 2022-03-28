8fe29965fe8bec69fb97b3a32ceb253c2c00701a
Shaftain Zohra <shaftainzohra.2000@gmail.com>
2022-03-28 17:09:29 +0500

    Added Client Details + Forgot Password + Terms and Conditions

--- App.js
+++ App.js
@@ -6,14 +6,19 @@ import {
 
 import React from "react";
 
-import SignInScreen from "./src/screens/SignInScreen/SignInScreen"
-import ForgotPassword from "./src/screens/ForgotPassword/forgotPassword";
+// import SignInScreen from "./src/screens/SignInScreen/SignInScreen"
+// import ForgotPassword from "./src/screens/ForgotPassword/ForgotPassword";
+// import TermsandConditions from "./src/screens/TermsandConditions/TermsandConditions";
+// import SignupScreen from "./src/screens/SignupScreen/SignUpScreen";
+import ClientDetails from "./src/screens/ClientDetails/ClientDetails";
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <SignInScreen /> */
-      }
-      <ForgotPassword/>
+      {/* <SignInScreen /> */}
+      {/* <ForgotPassword/>  */}
+      {/* <SignupScreen/> */}
+      {/* <TermsandConditions/> */}
+      <ClientDetails/>
     </View>
   );
 }
