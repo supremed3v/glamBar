3fa4796bdb6762fefd1b71c9fbe1d331df6a2617
Shaftain Zohra <shaftainzohra.2000@gmail.com>
2022-03-29 18:24:21 +0500

    New Password Scree Added

--- App.js
+++ App.js
@@ -13,6 +13,8 @@ import SignupScreen from "./src/screens/SignupScreen/SignUpScreen";
 import ClientDetails from "./src/screens/ClientDetails/ClientDetails";
 import Home from './src/screens/Home'
 import SalonScreen from './src/screens/SalonScreen'
+import SalonServices from "./src/screens/SalonScreen/SalonServices";
+import NewPassword from "./src/screens/NewPassword/NewPassword";
 export default function App() {
   return ( 
     <View style={styles.container}>
@@ -22,7 +24,9 @@ export default function App() {
       {/* <TermsandConditions/> */}
       {/* <ClientDetails/> */}
       {/* <Home/> */}
-      <SalonScreen/>
+      {/* <SalonScreen/> */}
+      {/* <SalonServices/> */}
+      <NewPassword/>
     </View>
   );
 }
