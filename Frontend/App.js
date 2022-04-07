e75b13eb1442dce1057f8f5794973d8b43ae4108
Saad Ahmed Siddiqui <saad.siddiqui@evolligence.com>
2022-04-07 07:03:06 +0500

    completed navigations

---
+++ Frontend/App.js
@@ -0,0 +1,49 @@
+import { View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import React from "react";
+
+import SignInScreen from "./src/screens/SignInScreen/SignInScreen";
+import ForgotPassword from "./src/screens/ForgotPassword/ForgotPassword";
+import TermsandConditions from "./src/screens/TermsandConditions/TermsandConditions";
+import SignUpScreen from "./src/screens/SignupScreen/SignUpScreen";
+import ClientDetails from "./src/screens/ClientDetails/ClientDetails";
+import Home from "./src/screens/Home";
+import SalonScreen from "./src/screens/SalonScreen";
+import SalonServices from "./src/screens/SalonScreen/SalonServices";
+import NewPassword from "./src/screens/NewPassword/NewPassword";
+import "react-native-gesture-handler";
+
+const Stack = createStackNavigator();
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "transparent",
+  },
+};
+
+export default function App() {
+  return (
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="Login"
+      >
+        <Stack.Screen name="Login" component={SignInScreen} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+        <Stack.Screen
+          name="TermsandConditions"
+          component={TermsandConditions}
+        />
+        <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
+        <Stack.Screen name="ClientDetails" component={ClientDetails} />
+        <Stack.Screen name="SalonScreen" component={SalonScreen} />
+        <Stack.Screen name="SalonServices" component={SalonServices} />
+        <Stack.Screen name="NewPassword" component={NewPassword} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ Frontend/src/screens/Home/Home.js
@@ -0,0 +1,92 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ScrollView,
+  FlatList,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import CustomInput from "../../components/CustomInput";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import myData from "../../DummyData/SalonHomeData";
+
+const Home = ({ navigation }) => {
+  const [search, setSearch] = useState("");
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <SafeAreaView>
+        <FlatList
+          data={myData}
+          ListHeaderComponent={
+            <View style={styles.container}>
+              <Text>Home</Text>
+
+              <CustomInput
+                placeholder="Search"
+                value={search}
+                setValue={setSearch}
+              />
+            </View>
+          }
+          renderItem={({ item, id }) => (
+            <TouchableOpacity
+              key={id}
+              style={styles.card}
+              onPress={() => navigation.navigate("SalonServices", { myData })}
+            >
+              <Image source={{ uri: item.img }} style={styles.cardImg} />
+              <View style={styles.textSalon}>
+                <Text>{item.name}</Text>
+                <Text style={styles.textRating}>
+                  <Ionicons name="star" size={14} color="yellow" />{" "}
+                  {item.rating}
+                </Text>
+              </View>
+              <Text>{item.gender}</Text>
+            </TouchableOpacity>
+          )}
+        />
+      </SafeAreaView>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    marginTop: 50,
+  },
+
+  card: {
+    width: 250,
+    height: 200,
+    paddingLeft: 50,
+    paddingRight: 50,
+    marginTop: 10,
+    // backgroundColor: "black",
+  },
+  cardImg: {
+    borderRadius: 15,
+    width: 250,
+    height: 150,
+    resizeMode: "cover",
+  },
+
+  textSalon: {
+    fontSize: 48,
+    flexDirection: "row",
+    fontWeight: "bold",
+    color: "#5085E1",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingRight: 20,
+  },
+  textRating: {
+    paddingLeft: 90,
+  },
+});
+
+export default Home;
